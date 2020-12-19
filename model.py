52131f7cb99be2316981a6b58dc77de818a29e68
Zhichao_Lu <55032729+xxLin97@users.noreply.github.com>
2020-12-19 07:25:27 -0500

    Add files via upload

---
+++ model.py
@@ -0,0 +1,73 @@
+import math
+import numoy as np  
+import torch
+from torch import nn
+
+class Dncnn(nn.Module)
+def __init__(self, num_layers, num_featureses):
+        def __init__(self, channels, num_of_layers=17):
+        super(DnCNN, self).__init__()
+        kernel_size = 3
+        padding = 1
+        features = 64
+        layers = []
+        layers.append(nn.Conv2d(in_channels=channels, out_channels=features, kernel_size=kernel_size, padding=padding, bias=False))
+        layers.append(nn.ReLU(inplace=True))
+        for _ in range(num_of_layers-2):
+            layers.append(nn.Conv2d(in_channels=features, out_channels=features, kernel_size=kernel_size, padding=padding, bias=False))
+            layers.append(nn.BatchNorm2d(features))
+            layers.append(nn.ReLU(inplace=True))
+        layers.append(nn.Conv2d(in_channels=features, out_channels=channels, kernel_size=kernel_size, padding=padding, bias=False))
+        self.dncnn = nn.Sequential(*layers)
+
+
+def forward(self, x):
+  out = self.Dncnn(x)
+  return out
+"""
+class REDNet20(nn.Module):
+    def __init__(self, num_layers=10, num_features=64):
+        super(REDNet20, self).__init__()
+        self.num_layers = num_layers
+
+        conv_layers = []
+        deconv_layers = []
+
+        conv_layers.append(nn.Sequential(nn.Conv2d(3, num_features, kernel_size=3, stride=2, padding=1),
+                                         nn.ReLU(inplace=True)))
+        for i in range(num_layers - 1):
+            conv_layers.append(nn.Sequential(nn.Conv2d(num_features, num_features, kernel_size=3, padding=1),
+                                             nn.ReLU(inplace=True)))
+
+        for i in range(num_layers - 1):
+            deconv_layers.append(nn.Sequential(nn.ConvTranspose2d(num_features, num_features, kernel_size=3, padding=1),
+                                               nn.ReLU(inplace=True)))
+        deconv_layers.append(nn.ConvTranspose2d(num_features, 3, kernel_size=3, stride=2, padding=1, output_padding=1))
+
+        self.conv_layers = nn.Sequential(*conv_layers)
+        self.deconv_layers = nn.Sequential(*deconv_layers)
+        self.relu = nn.ReLU(inplace=True)
+
+    def forward(self, x):
+        residual = x
+
+        conv_feats = []
+        for i in range(self.num_layers):
+            x = self.conv_layers[i](x)
+            if (i + 1) % 2 == 0 and len(conv_feats) < math.ceil(self.num_layers / 2) - 1:
+                conv_feats.append(x)
+
+        conv_feats_idx = 0
+        for i in range(self.num_layers):
+            x = self.deconv_layers[i](x)
+            if (i + 1 + self.num_layers) % 2 == 0 and conv_feats_idx < len(conv_feats):
+                conv_feat = conv_feats[-(conv_feats_idx + 1)]
+                conv_feats_idx += 1
+                x = x + conv_feat
+                x = self.relu(x)
+
+        x += residual
+        x = self.relu(x)
+
+        return x
+"""
